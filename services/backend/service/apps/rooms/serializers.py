15b58b564f5a43ee8cf6045b74abac016c556e4b
night <artyra5055@gmail.com>
2023-06-06 22:58:23 +0300

    patch error

--- services/backend/service/apps/rooms/serializers.py
+++ services/backend/service/apps/rooms/serializers.py
@@ -17,34 +17,18 @@ class RoomSerializers(serializers.ModelSerializer):
 
 
 class RentSerializers(serializers.ModelSerializer):
-    room = RoomSerializers(read_only=True)
+    room = RoomSerializers()
     user = user_serializers.UserSerializers(read_only=True)
 
     class Meta:
         model = Rent
         fields = (
             'uuid',
-            'room',
             'user',
             'time_start',
             'time_end',
+            'room'
         )
-
-    def validate(self, data):
-        room = data.get('room')
-        time_start = data.get('time_start')
-        time_end = data.get('time_end')
-
-        rents = Rent.objects.filter(
-            room=room,
-            time_start__lt=time_end,
-            time_end__gt=time_start,
-        )
-
-        if rents.exists():
-            raise serializers.ValidationError('This time range is already reserved for this room')
-
-        return data
 
 
 class RentCreateSerializers(serializers.ModelSerializer):
